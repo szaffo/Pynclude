ede559d4faee4c3372781a5ed9f5178251bc5615
Szabó <martin77szabo@gmail.com>
2019-04-10 13:42:13 +0200

    groups are named in regexes

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ CWD = os.getcwd()
 FILENAME = "main.py"
 FILEPATH = CWD + "/" + FILENAME
 # ===================================================
-RE_INCLUDE = re.compile(r"^\s*# ?pynclude\s*([A-z\./aáoóöőuúüű0-9]+\.py)\s*$")
-RE_SETDIR = re.compile(r"^\s*# ?setdir\s*(?P<path>[A-z\./aáoóöőuúüű0-9]+(/)?)\s*$")
-RE_DEFINE = re.compile(r"^\s*# ?define\s*([A-zaáoóöőuúüű0-9]+)\s*$")
-RE_IFDEFINED = re.compile(r"^\s*# ?ifdef\s*([A-zaáoóöőuúüű0-9]+)\s*$")
-RE_IFNDEFINED = re.compile(r"^\s*# ?ifndef\s*([A-zaáoóöőuúüű0-9]+)\s*$")
+RE_INCLUDE = re.compile(r"^\s*# ?pynclude\s*(?P<data>[A-z\./aáoóöőuúüű0-9]+\.py)\s*$")
+RE_SETDIR = re.compile(r"^\s*# ?setdir\s*(?P<data>[A-z\./aáoóöőuúüű0-9]+(/)?)\s*$")
+RE_DEFINE = re.compile(r"^\s*# ?define\s*(?P<data>[A-zaáoóöőuúüű0-9]+)\s*$")
+RE_IFDEFINED = re.compile(r"^\s*# ?ifdef\s*(?P<data>[A-zaáoóöőuúüű0-9]+)\s*$")
+RE_IFNDEFINED = re.compile(r"^\s*# ?ifndef\s*(?P<data>[A-zaáoóöőuúüű0-9]+)\s*$")
 RE_ELSE = re.compile(r"^\s*# ?else\s*$")
 RE_COMMENT = re.compile(r"^\s*#.*$")
 # ===================================================
