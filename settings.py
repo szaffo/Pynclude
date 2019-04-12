4319c09e1612b2ff71915ea6e6635d89c63daeb1
Szabó <martin77szabo@gmail.com>
2019-04-12 11:02:05 +0200

    creates settings

---
+++ settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import os
+import json
+
+cwd = os.getcwd() + "/"
+
+settings = {
+    "recursive": True,
+    "keep_comments": True,
+    "input_file": "main.py",
+    "output_file": "builded.py",
+    "inlude_directory": cwd,
+    "cwd": cwd,
+    "verbose": True
+}
+
+with open('settings.pynclude', 'w') as outfile:
+    json.dump(settings, outfile, indent=4)
